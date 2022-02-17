021b4a41c98c39a3791d77d0ec0f5f70dac3e3ce
rahk1990 <rahkem.ellis@gmail.com>
2022-02-17 16:11:03 -0500

    created serializers for supers and super_types classes.

---
+++ super_types/serializer.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Super_types
+
+class Super_typesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Super_types
+        fields = ['id','villain', 'hero']
