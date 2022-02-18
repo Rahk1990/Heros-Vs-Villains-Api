cb94d696082593170c3f4744dd1c10998487ba43
rahk1990 <rahkem.ellis@gmail.com>
2022-02-18 14:27:02 -0500

    created urls for 'supers' and 'super_types'updated syntax for forementioned modules and files.

--- super_types/views.py
+++ super_types/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+from .serializers import Super_typesSerializer
+from .models import Super_types
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
+
+@api_view(['GET'])
+def super_types_list(resquest):
+
+
+    return Response('ok')
--- supers/views.py
+++ supers/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+from .serializers import SupersSerializer
+from .models import Supers 
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
+
+@api_view(['GET'])
+def supers_list(resquest):
+
+
+    return Response('ok')
